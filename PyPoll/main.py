8b3180e1ad2e0d8f9a14db1cfc51835ea7be802a
jeodaysju <jeodaysju@yahoo.com>
2020-07-07 15:56:15 -0500

    PyPoll portion working

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,23 +4,32 @@ import csv
 # Path to collect data from the Resources folder
 election_csv = os.path.join('Resources', 'election_data.csv')
 
+# files to load and output
+file_to_output = os.path.join("analysis", "election_analysis.txt")
+
+total_votes = 0
+candidates = {}
+
 # Read in the CSV file
 with open(election_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     # Read the header row first (skip this step if there is now header)
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    # print(f"CSV Header: {csv_header}")
 
-    for rows in csvreader:
-        # print(rows)
-        # election_dict{}
-        election_dict = {'voter_id': rows[0],
-                    'county': rows[1],
-                    'candidate': rows[2]
-                    }
-        print()
-        # election_dict = dict(voter_id = rows[0],
-        #             county = rows[1],
-        #             candidate = rows[2])
+    for row in csvreader:
+        # calc total votes
+        total_votes = total_votes + 1
 
-        print(f'{election_dict['county'][20]}')
+        name = row[2]
+
+        if name not in candidates:
+            candidates[name] = 1
+        else:
+            candidates[name] = candidates[name] + 1
+
+print(total_votes)
+
+for candidate_name, vote_count in candidates.items():
+        print(f"{candidate_name}: {vote_count}")
+
